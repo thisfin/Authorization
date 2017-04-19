03f28229e2875ef8230725e27083161246c3720f
文优 <thisfin@gmail.com>
2017-04-19 16:16:10 +0800

    Update

---
+++ Authorization/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Authorization
+//
+//  Created by wenyou on 2017/4/19.
+//  Copyright © 2017年 wenyou. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Authorization/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Authorization
+//
+//  Created by wenyou on 2017/4/19.
+//  Copyright © 2017年 wenyou. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
